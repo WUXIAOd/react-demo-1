db798b3e08a9ac25a4a0d77e37a9c0d554e520fd
wuxiao <1291383707@qq.com>
2019-11-29 16:49:59 +0800

    react初体验

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const div = (
+    React.createElement('div',null,
+        React.createElement('p',null,
+            React.createElement('span',null,'hi')))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
+//等价于：
+const div = (
+    <div>
+        <p>
+            <span>hi</span>
+        </p>
+    </div>
+)
+console.log(div);
+ReactDOM.render(div,document.getElementById('root'))
+
+
